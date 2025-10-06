3d887d9eb9d08c0065c54852bbb4e67b4203c0e6
cristina.madrid.crespo.contractor <cristina.madrid.crespo.contractor@bbva.com>
2025-10-06 12:28:45 +0200

    portfolio_date not in clan_date value

--- joysticksecuritizatiotl3swp/app.py
+++ joysticksecuritizatiotl3swp/app.py
@@ -6,7 +6,6 @@ import pyspark.sql.functions as F
 from alfred.logging.tracing import ExecutionLogger
 
 from joysticksecuritizatiotl3swp.executor.main import SecuritizationProcess
-from joysticksecuritizatiotl3swp.read.paths import Paths
 from joysticksecuritizatiotl3swp.utils.utilities import Utilities
 
 
@@ -48,26 +47,14 @@ class DataprocExperiment:
                 sandbox_path = parameters["SANDBOX_PATH_DEV"] if parameters[
                     "OUTPUT_MODE"] == "DEVELOPMENT" else parameters["SANDBOX_PATH_PROD"]
 
-                last_date_available_limits = (
-                    Utilities.get_last_value_partition_table(
-                        parameters["LIMITS_LAUNCHPAD_TABLE"],
-                        "gf_loaded_limits_list_date")
-                )
-
-                portfolio_date = (
-                    dataproc.read().table(parameters["LIMITS_LAUNCHPAD_TABLE"])
-                    .filter(F.col("gf_loaded_limits_list_date") == last_date_available_limits)
-                    .filter(F.col("gf_loaded_limits_list_category_name") == "portfolio_date")
-                    .select("gf_first_concept_desc")
-                    .collect()[0][0]
-                ).replace("-", "")
+                clan_date = parameters["CLAN_DATE"]
 
                 if (Utilities.check_for_partition(
-                        f"{sandbox_path}mrr/joystick_mrr", "clan_date", portfolio_date)):
+                        f"{sandbox_path}mrr/joystick_mrr", "clan_date", clan_date)):
                     cubo_df = (
                         dataproc.read()
                         .option("basePath", f"{sandbox_path}mrr/joystick_mrr")
-                        .parquet(f"{sandbox_path}mrr/joystick_mrr/clan_date={portfolio_date}")
+                        .parquet(f"{sandbox_path}mrr/joystick_mrr/clan_date={clan_date}")
                     )
                     SecuritizationProcess(
                         self.logger, spark, dataproc, parameters
@@ -75,7 +62,7 @@ class DataprocExperiment:
 
                 else:
                     self.logger.info(
-                        "Portfolio date %s not found in MRR partition. Executing full engine." % portfolio_date)
+                        "Clan date %s not found in MRR partition. Executing full engine." % clan_date)
                     SecuritizationProcess(self.logger, spark,
                                           dataproc, parameters).execute_process()
 
